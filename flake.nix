1cee73e7b70157719cf75587c912d9c42f0afa0f
Tomas Zaluckij <me@tomaszal.com>
2024-04-02 00:29:00 +0100

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            # Nix tools
+            alejandra
+            nixd
+          ];
+        };
+      };
+    };
+}
